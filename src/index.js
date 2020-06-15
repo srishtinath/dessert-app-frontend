cb0f3333190824194aeb495da560dfbddab3fb15
Srishti Nath <srishti.nath@gmail.com>
2020-06-15 17:11:44 -0400

    additional drag

--- src/index.js
+++ src/index.js
@@ -18,28 +18,53 @@ document.addEventListener("DOMContentLoaded", function(){
 
     function renderIngredient(ingredient){
         let ingredientDiv = document.createElement('div')
+        ingredientDiv.dataset.image = ingredient.image
         ingredientDiv.className = "ing-div"
         ingredientDiv.innerHTML = `
         <img src="assets/images/${ingredient.image}" class="ing-img">
         <br>${ingredient.name}`
         pantryContainer.append(ingredientDiv)
-        // clickAndDrag(ingredientDiv)
+        ingredientDiv.draggable = true
     }
 
     fetchIngredients()
-  
+
+    function grabData(){
+        const ingDivs = document.getElementsByClassName('ing-div')
+        console.log(ingDivs)
+        ingDivs.forEach(ingDiv => {
+            console.log(ingDiv)
+            ingDiv.addEventListener('ondragstart', function(e){
+                console.log("from event listener")
+            })
+        })
+    }
+        
+    grabData()
+
+    // function dragInfo(ingDiv){
+    //     ingDiv.addEventListener('ondragstart', function(e){
+    //         console.log(e.target)
+    //     })
+    //     ingDiv.addEventListener('ondrag', function(e){
+    //         console.log("ondrag")
+    //     })
+    // }
+
     function preventDefault(e) {
         e.preventDefault();
         e.stopPropagation();
     }
 
     function handleDrop(e) {
-        console.log(e.target)
-        const imgList = document.getElementsByClassName("ing-list")[0]
-        const imgAdded = document.createElement('img')   
-        imgAdded.className = "ing-img"
-        imgAdded.src = "assets/images/vanilla.png"
-        imgList.append(imgAdded)
+        // const data = e.dataTransfer.getData("text", e.target.dataset.image)
+        console.log(e.target.dataset)
+        // console.log(data)
+        // const imgList = document.getElementsByClassName("ing-list")[0]
+        // const imgAdded = document.createElement('img')   
+        // imgAdded.className = "ing-img"
+        // imgAdded.src = "assets/images/vanilla.png"
+        // imgList.append(imgAdded)
     }
  
     ingContainer.addEventListener('dragenter', preventDefault, false);
